3352ab046a648421a0ecb8a1859e91e0edaab086
Sri Sai Poosarla <sairampoosarlahyd@gmail.com>
2025-05-03 13:21:33 +1000

    code for saving the file in cloud is added

--- app.js
+++ app.js
@@ -17,8 +17,16 @@ const productRouter = require('./routes/productRoutes')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+//cloudinary
+const cloudinary = require('cloudinary').v2
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+})
+
 app.use(express.json());
-app.use(fileUpload())
+app.use(fileUpload({useTempFile: true}))
 app.use(express.static('./public'))
 
 app.get('/', (req, res) => {
--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -2,9 +2,10 @@ const fileUpload = require('express-fileupload')
 const path = require('path')
 const {StatusCodes} = require("http-status-codes")
 const CustomError = require('../errors')
+const cloudinary = require('cloudinary').v2
 
-const uploadProductImage = async (req, res) =>{
-    console.log(req.files)
+//this function will take the incoming file and save it to the local folder
+const uploadProductImageLocal = async (req, res) =>{
 
     //check for file
     if(!req.files.image){
@@ -21,11 +22,21 @@ const uploadProductImage = async (req, res) =>{
     if(productimage.size > 1024*1024){
         throw new CustomError.BadRequestError('File uploaded is larger then 1MB')
     }
-    
 
     const imagePath = path.join(__dirname, '../public/uploads/'+req.files.image.name)
     await productimage.mv(imagePath)
     res.status(StatusCodes.OK).json({image:{src:`/uploads/${req.files.image.name}`}})
+}
+
+//this function will save the incoming file into a temp folder and 
+//then the file is sent to cloudinary to save it
+const uploadProductImage = async (req,res) => {
+    const result = await cloudinary.uploader.upload(req.files.image.tempFilePath,{
+        use_filename: true,
+        folder:'file-upload',
+    })
+
+    return res.status(StatusCodes.OK).json({image:{src:result.secure_url}})
 }
 
 module.exports = {uploadProductImage}
